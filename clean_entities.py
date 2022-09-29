4374b56e62e3ce5b83c3514f579fa783f3c6b76b
Greg Adair <gadair@gmail.com>
2022-09-28 17:25:13 -0700

    clean entities by type as a parameter update for new config style

--- clean_entities.py
+++ clean_entities.py
@@ -1,13 +1,21 @@
-import knowledge_server
+import argparse
+import knowledge_finder
 
 if __name__ == "__main__":
-    conn = knowledge_server.KnowledgeConnection('knowledge_config.json')
+    parser = argparse.ArgumentParser(description='Sync entities between Senzing and ArcGIS Knowledge')  
+    parser.add_argument('-c', '--config', type=str, default='knowledge_config.json')
+    parser.add_argument('-t', '--type', type=str, required=True)
+    args = parser.parse_args()
+
+    #add knowledge protobuf python lib to the pythonpath
+    knowledge_finder.findKnowledge(args.config)
+
+    import knowledge_server
+    conn = knowledge_server.KnowledgeConnection(args.config)
     kapi = knowledge_server.KnowledgeAPI(conn)
     szfunc = knowledge_server.SenzingKnowledgeFunctions(kapi)
 
-    print('deleting all resolved entities')
-    szfunc.deleteAllEntitiesByType('person_resolved_entity', True)
+    print(F'deleting all entities of type {args.type}')
+    szfunc.deleteAllEntitiesByType(args.type, True)
 
-#    print('deleting all records')
-#    szfunc.deleteAllEntitiesByType('senzing_record', True)
   
